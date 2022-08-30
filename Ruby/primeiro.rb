9b587c07a8e447f0a4e829a742d6112f6b857089
Jean Porto <portojean@gmail.com>
2022-08-30 19:53:28 -0300

    add exercicio

---
+++ Ruby/primeiro.rb
@@ -0,0 +1,14 @@
+#irá imprimir na tela
+puts "Hello World"
+
+nome ="Jean"
+
+puts nome[0,3].upcase 
+puts nome[0,3].downcase 
+puts nome.capitalize 
+
+p nome.chars
+
+m = "ola nome"
+puts m
+puts m.gsub("nome", "Jean")
