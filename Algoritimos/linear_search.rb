7b5939a4364dcb5b939ae5132a339b6dd1dbc795
Jean Porto <portojean@gmail.com>
2022-09-07 16:55:22 -0300

    linear certo

--- Algoritimos/linear_search.rb
+++ Algoritimos/linear_search.rb
@@ -3,9 +3,8 @@
 arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
 x = 110
 
-    arr.each{
-        |i|
-        if i == x  
-            puts i
-        end
-    }
+arr.each do |i|
+    if i === x
+        puts arr.index(i)  
+    end        
+end
