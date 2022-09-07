93a0cf6200f5813685b4b3eca93cd5116005826a
Jean Porto <portojean@gmail.com>
2022-09-07 17:27:29 -0300

    desenvolvendo os outros parametro de busca

--- Algoritimos/linear_search.rb
+++ Algoritimos/linear_search.rb
@@ -2,9 +2,13 @@
 
 arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
 x = 110
+n = arr.length
+puts n
+
 
 arr.each do |i|
     if i === x
         puts arr.index(i)  
-    end        
+    end
 end
+
