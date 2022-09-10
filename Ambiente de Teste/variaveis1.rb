4a7e470c380665f765cd3c73755e6095d4f32bdb
Jean Porto <portojean@gmail.com>
2022-09-10 19:27:18 -0300

    mais variaveis

---
+++ Ambiente de Teste/variaveis1.rb
@@ -0,0 +1,12 @@
+class Exemplo
+    PI = 3.1415
+    VAR2 = 200
+    def show 
+        puts "Valor da primeira Constante: #{PI}"
+        puts "Valor da primeira Constante: #{VAR2}"
+        
+    end
+end
+
+obj1 = Exemplo.new()
+obj1.show
