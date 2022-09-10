add9293ead0b8527348bbd1dcc04b01ad9176a01
Jean Porto <portojean@gmail.com>
2022-09-10 07:33:18 -0300

    rout

--- teste/config/routes.rb
+++ teste/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users
   get 'usuarios', to: 'users#index'
+  #get 'index', to: 'index#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
