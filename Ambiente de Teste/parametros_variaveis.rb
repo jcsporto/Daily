9f784853a94482bfe9999ad48bff2dbcbfbb06a5
Jean Porto <portojean@gmail.com>
2022-09-12 06:03:29 -0300

    add mais exercicios

--- Ambiente de Teste/parametros_variaveis.rb
+++ Ambiente de Teste/parametros_variaveis.rb
@@ -14,3 +14,11 @@ end
 sample "Jean", 40, "M"
 sample "Poli", "35", "F", "Professora"
 sample "Rafaella", 3, "F", "Maternal", "CEMEI"
+
+
+def test
+    yield 5
+    puts "You are in the method test"
+    yield 100
+ end
+ test {|i| puts "You are in the block #{i}"}
---
+++ Ambiente de Teste/test.rb
@@ -0,0 +1,5 @@
+
+arr = [10, 20, 30]
+arr.each do |i|
+    puts arr[i]
+end
