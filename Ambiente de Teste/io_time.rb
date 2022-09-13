43ff89cbeca092aa383cdc36fc67bac827f256d7
Jean Porto <portojean@gmail.com>
2022-09-13 07:29:13 -0300

    add io_time

---
+++ Ambiente de Teste/io_time.rb
@@ -0,0 +1,3 @@
+File::ctime( "test.txt" ) # => Fri May 09 10:06:37 -0700 2008
+File::mtime( "text.txt" ) # => Fri May 09 10:44:44 -0700 2008
+File::atime( "text.txt" ) # => Fri May 09 10:45:01 -0700 2008
