f73adfd3774bd616508f5edc35fb6d44c35feac1
Jean Porto <portojean@gmail.com>
2022-09-18 17:00:12 -0300

    incrementando

--- Algoritimos/linear_search.rb
+++ Algoritimos/linear_search.rb
@@ -2,13 +2,19 @@
 
 arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
 x = 110
+cont = 0
 n = arr.length
 puts n
 
 
+
+
 arr.each do |i|
     if i === x
-        puts arr.index(i)  
+        puts arr.index(i) 
+        cont += 1
+        puts "Contador: #{cont}"        
     end
 end
+
 
