40ed1ffd2d6f0790e44659318dc4b0125f71c089
Jean Porto <portojean@gmail.com>
2022-09-19 09:55:54 -0300

    add

--- Algoritimos/linear_search.rb
+++ Algoritimos/linear_search.rb
@@ -1,19 +1,17 @@
 #busca linear
+#Dado um array arr[] de N elementos, 
+#a tarefa é escrever uma função para pesquisar um dado elemento x em arr[] .
 
 arr = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
 x = 110
 cont = 0
 n = arr.length
-puts n
-
-
 
 
 arr.each do |i|
     if i === x
         puts arr.index(i) 
-        cont += 1
-        puts "Contador: #{cont}"        
+    else puts "-1"       
     end
 end
 
