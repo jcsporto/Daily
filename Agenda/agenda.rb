9bc8e353173a106a7583797e2b608bcdb1f53bb5
Jean Porto <portojean@gmail.com>
2022-09-20 07:02:16 -0300

    iniciando agenda

---
+++ Agenda/agenda.rb
@@ -0,0 +1,14 @@
+# Adicionar, Editar e Remover Contato
+# Contatos terão as seguintes informações: nome e telefone
+# Poderemos ver todos os contatos registrados ou somente um contato
+
+
+loop do 
+puts "1. Contatos\n2. Adicionar Contato\n3. Ver Contato\n4. Editar Contato\n5. Remover Contato\n0. Sair"
+
+    case 
+    when codigo == 0
+        puts "Até mais!"
+        break
+    end
+end
