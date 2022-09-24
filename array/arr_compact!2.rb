2755ce8ad20b7ecb978a3e6b4fc6b277b79d6474
Jean Porto <portojean@gmail.com>
2022-09-24 18:26:53 -0300

    arr compactsudo apt-get install update

--- array/arr_compact!2.rb
+++ array/arr_compact!2.rb
@@ -1,9 +1,9 @@
 # compact remove nil do array
 # Syntax:  Array.compact()
 
-a = [18, 22, 33, nil, 5, 6]
-b = [5, 4, 1, 88, 9]
-c = [18, 22, nil, 40, 50, 6]
+a = ["abc", "nil", "dog"]
+b = ["cow", nil, "dog"]
+c = ["cat", nil, nil]
 
 puts "Removing nil value:  #{a.compact!}\n\n"
 
