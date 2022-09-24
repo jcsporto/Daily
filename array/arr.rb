ebfeb4aa3ae4bf1f5304745c800a0f45cb80aaba
Jean Porto <portojean@gmail.com>
2022-09-24 18:12:11 -0300

    array - compact

---
+++ array/arr.rb
@@ -0,0 +1,12 @@
+# compact remove nil do array
+# Syntax:  Array.compact()
+
+a = [18, 22, 33, nil, 5, 6]
+b = [5, 4, nil, 1, 88, 9]
+c = [18, 22, nil, 40, 50, 6]
+
+puts "Removing nil value:  #{a.compact}\n\n"
+
+puts "Removing nil value:  #{b.compact}\n\n"
+
+puts "Removing nil value:  #{c.compact}\n\n"
