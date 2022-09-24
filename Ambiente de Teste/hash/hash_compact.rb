312d3123058bad457312329d7a7dccc7b3050f05
Jean Porto <portojean@gmail.com>
2022-09-24 18:35:15 -0300

    hash compact!

---
+++ Ambiente de Teste/hash/hash_compact.rb
@@ -0,0 +1,13 @@
+#Hash compact!
+
+a = {a:100, b:nil}
+
+b = {a:100, b:nil, c:200 }
+
+c = {a:100}
+
+puts "removing nil value: #{a.compact!}\n\n"
+
+puts "removing nil value: #{b.compact!}\n\n"
+
+puts "removing nil value: #{c.compact!}\n\n"
