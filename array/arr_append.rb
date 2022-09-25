54d29e192bfe5b041296b5678efdb244069d6656
Jean Porto <portojean@gmail.com>
2022-09-25 07:55:25 -0300

    arr append incompleto

---
+++ array/arr_append.rb
@@ -0,0 +1,12 @@
+#Array#append() é um método da classe Array que adiciona elementos no final do array
+
+a = [18, 22, 33, 4, 5, 6]
+b = [5, 4, 22, 1, 88, 9]
+c = [18, 22, 33, 40, 50, 6]
+   
+
+puts "adding elements in a : #{a.append(b)}\n\n"
+   
+puts "adding elements in b : #{b.append("ratttt")}\n\n"
+   
+puts "adding elements in c : #{c.append(b)}\n\n"
