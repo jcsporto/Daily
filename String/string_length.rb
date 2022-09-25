6681645a9ca93723955b27274db304f54755ce06
Jean Porto <portojean@gmail.com>
2022-09-25 07:48:13 -0300

    string length

---
+++ String/string_length.rb
@@ -0,0 +1,10 @@
+# length é um método de classe String em Ruby que é usado para encontrar 
+# o comprimento de caractere de uma determinada string.
+
+puts "GFG".length
+puts "geeksforgeeks".length
+puts "-----------------"
+puts "g4g".length
+puts "ruby string".length
+puts "-----------------"
+puts "Jean Carlo Santos Porto".length
