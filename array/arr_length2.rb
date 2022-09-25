c24b1fc3e84f7cec692add27db5a8277e41e3bef
Jean Porto <portojean@gmail.com>
2022-09-25 07:54:36 -0300

    arr length2

---
+++ array/arr_length2.rb
@@ -0,0 +1,12 @@
+#Array#length() : length() é um método da classe Array que retorna o número de elementos no array
+
+
+a = ["abc", "nil", "dog"]  
+c = ["cat", nil]
+b = ["cow", nil, "dog"]
+
+puts "length() method form: #{a.length()}\n"
+
+puts "length() method form: #{b.length()}\n"
+
+puts "length() method form: #{c.length()}\n"
