6cc5c57b8576e8095df3a20b0167c68ce839dd98
Jean Porto <portojean@gmail.com>
2022-09-28 08:56:30 -0300

    each

---
+++ NB/each.rb
@@ -0,0 +1,14 @@
+nomes = ["Jean", "Maria", "José", "Mateus"]
+
+dict = {nome: "Diego", idade: 35, altura: 1.75}
+
+nome = "Diego"
+# nomes.each do |nome|
+#     puts nome
+# end
+
+for nome in nomes do
+    puts nome
+end
+
+puts nome 
