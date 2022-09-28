88dd152a4117ddae8d5efcbf77c97d9c4b4f4e7b
Jean Porto <portojean@gmail.com>
2022-09-28 07:15:46 -0300

    add hashes

---
+++ NB/hashes.rb
@@ -0,0 +1,13 @@
+hash = {nome: "Jean", idade: 35}
+hash[:altura] = 1.84
+#hash.delete(:altura)
+puts hash.keys
+puts hash.size
+puts hash.length 
+puts hash.has_value?("Jean")
+
+
+# hash1 = {:um =>1, :dois =>2, :tres => 3}
+# hash2 = { :tres => 3, :quatro => 4, :cinco=>5}
+
+# puts hash1 == hash2
