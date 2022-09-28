0684dcb326886f0beca223bf4187b8786eec1940
Jean Porto <portojean@gmail.com>
2022-09-28 09:11:36 -0300

    each alteracao

--- NB/each.rb
+++ NB/each.rb
@@ -2,13 +2,16 @@ nomes = ["Jean", "Maria", "José", "Mateus"]
 
 dict = {nome: "Diego", idade: 35, altura: 1.75}
 
-nome = "Diego"
+chave = "Diego"
 # nomes.each do |nome|
 #     puts nome
 # end
 
-for nome in nomes do
-    puts nome
+dict.each do |chave, valor|
+    puts "#{chave}: #{valor}"
 end
+# for nome in nomes do
+#     puts nome
+# end
 
-puts nome 
+puts chave
