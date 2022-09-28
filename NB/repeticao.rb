484f5b7948917730a75fd5e4f5b3dfa6a9e3b8dd
Jean Porto <portojean@gmail.com>
2022-09-28 08:56:59 -0300

    repeticao

---
+++ NB/repeticao.rb
@@ -0,0 +1,11 @@
+nomes = ["Joao", "Jean", "Maria", "Pedro", "Anna"]
+
+dict = {nome: "Diego", idade: 32, altura: 1.75}
+
+# for nome in nomes do 
+#     puts nome
+# end
+
+for k, v in dict do
+    puts "#{k}: #{v}"
+end
