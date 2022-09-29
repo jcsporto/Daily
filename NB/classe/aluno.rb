8dcfb499d5ce5dd61bb266e444f274129f25607a
Jean Porto <portojean@gmail.com>
2022-09-29 06:23:55 -0300

    initialize

--- NB/classe/aluno.rb
+++ NB/classe/aluno.rb
@@ -1,22 +1,16 @@
 class Aluno
-    attr_accessor :nome, :idade
+    attr_reader :nome, :idade
     
-
-    # def mudar_nome(nome)
-    #     @nome = nome
-    # end
-
-    # def mostra_nome
-    #     @nome
-    # end
+    def def initialize(n, i)
+      @nome, @idade = n, i
+    end 
 end
 
 
-a1 = Aluno.new 
-# a1.mudar_nome("Jean")
-# # puts a1.mostra_nome
+a1 = Aluno.new ("Jean", 40)
 
-a1.nome = "Jean"
-a1.idade = "42"
+
+# a1.nome = "Jean"
+# a1.idade = "42"
 
 puts "Nome: #{a1.nome} - Idade: #{a1.idade}"
