72d2a4645851b732afffffc9e0a6395eab82b0b1
Jean Porto <portojean@gmail.com>
2022-09-29 05:45:42 -0300

    ternario

---
+++ NB/ternario.rb
@@ -0,0 +1,12 @@
+nome = "Jean"
+
+# if nome.eql?("Jean")
+#     puts "Estudando Ruby"
+# else 
+#     puts "Visitante"
+# end
+
+# ternario
+# condição ?  verdadeiro : falso
+
+puts nome.eql?("Jean") ? "Estudando RoR" : "Visitante"
