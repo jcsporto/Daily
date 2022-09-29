1dc7e24ce90a25f5cbb94ccc541b4d11b93232c0
Jean Porto <portojean@gmail.com>
2022-09-29 06:19:54 -0300

    attr_accessor

--- NB/classe/aluno.rb
+++ NB/classe/aluno.rb
@@ -1,17 +1,22 @@
 class Aluno
-    @nome
-    @idade
+    attr_accessor :nome, :idade
+    
 
-    def mudar_nome(nome)
-        @nome = nome
-    end
+    # def mudar_nome(nome)
+    #     @nome = nome
+    # end
 
-    def mostra_nome
-        @nome
-    end
+    # def mostra_nome
+    #     @nome
+    # end
 end
 
 
 a1 = Aluno.new 
-a1.mudar_nome("Jean")
-puts a1.mostra_nome
+# a1.mudar_nome("Jean")
+# # puts a1.mostra_nome
+
+a1.nome = "Jean"
+a1.idade = "42"
+
+puts "Nome: #{a1.nome} - Idade: #{a1.idade}"
