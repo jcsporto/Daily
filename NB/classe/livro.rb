44dd8137a52732a1f74254cd6bb5777f644cceb6
Jean Porto <portojean@gmail.com>
2022-10-02 05:52:09 -0300

    add livro

--- NB/classe/livro.rb
+++ NB/classe/livro.rb
@@ -1,4 +1,26 @@
 class Livro
-    attr_reader :
-
+    attr_reader :nome, :ano, :preco
     
+    def initialize(nome, ano, preco)
+      @nome = nome
+      @ano = ano
+      @preco = dar_desconto(preco)      
+    end
+
+    def mostrar_dados 
+        puts "Livro: #{@nome}, Ano Lançamento: #{@ano}, Preço: R$ #{@preco} "
+    end
+
+    private
+    def dar_desconto(preco)
+        if @ano < 2000
+            preco * 0.9
+        else
+            preco
+        end
+    end
+end
+
+l1 = Livro.new("O Senhor dos Anéis", 1999, 50.00)
+#l1.dar_desconto
+l1.mostrar_dados
