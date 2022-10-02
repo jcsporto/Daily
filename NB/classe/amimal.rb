e48239c9122cc9f20494467cb44f8cfa49a2db0b
Jean Porto <portojean@gmail.com>
2022-10-02 06:36:47 -0300

    add modulo

--- NB/classe/amimal.rb
+++ NB/classe/amimal.rb
@@ -1,4 +1,7 @@
+require_relative "modulo"
+
 class Animal
+    include Comunica
     attr_reader :nome
 
     def initialize(nome)
@@ -23,6 +26,23 @@ class Cachorro < Animal
     end
 end
 
+class Gato < Animal
+    attr_reader :raca
+
+    def initialize(nome, raca)
+        super(nome)
+        @raca = raca
+    end
+
+    def comunicar  #sobrescreve o metod comunicar da classe mãe
+        puts "Miau Miau!" 
+    end
+end
+
 bob = Cachorro.new("Bob", "Pitbull")
 puts "#{bob.nome} #{bob.raca}"
 puts "#{bob.comunicar}"
+puts "---------------------"
+xaninho = Gato.new("Xaninho", "Siamés")
+puts "#{xaninho.nome} #{xaninho.raca}"
+puts "#{xaninho.comunicar}"
