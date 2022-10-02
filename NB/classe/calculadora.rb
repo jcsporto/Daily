8338d3055f8e3bbe2ea6f34556beed552857f45d
Jean Porto <portojean@gmail.com>
2022-10-02 06:37:08 -0300

    add modulo

--- NB/classe/calculadora.rb
+++ NB/classe/calculadora.rb
@@ -1,4 +1,7 @@
+require_relative "modulo"
+
 class Calculadora
+    include Operacoes
     def somar (*args)
         lista = []
         lista.push(*args)
@@ -8,15 +11,17 @@ end
 
 c1 = Calculadora.new 
 resultado = c1.somar(5, 5, 10, 22, 15)
+res_subt = c1.subt(10, 5)
 puts resultado
+puts res_subt
 
-class Aluno
-    def notas(*args)
-        notas = []
-        notas.push(*args)
-        notas
-    end
-end
+# class Aluno
+#     def notas(*args)
+#         notas = []
+#         notas.push(*args)
+#         notas
+#     end
+# end
 
 a1 = Aluno.new
 notas_a1 = a1.notas(10, 7.5, 9.5)
