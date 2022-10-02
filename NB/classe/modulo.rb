e6ffc6bac6f0efb1f1f7a8ae9b2b17a1ff49dc23
Jean Porto <portojean@gmail.com>
2022-10-02 06:37:41 -0300

    add abstração

---
+++ NB/classe/modulo.rb
@@ -0,0 +1,11 @@
+module Comunica
+    def comunicar
+        puts "Ola, eu sou a classe mãe ou super classe como vc preferir"
+    end
+end
+
+module Operacoes
+    def subt(x, y)
+        x -y
+    end
+end
