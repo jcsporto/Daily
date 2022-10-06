4dc58e2d785416404ed8a7ce6e41f4c6c31b1374
Jean Porto <portojean@gmail.com>
2022-10-06 07:09:24 -0300

    add schema

--- NB/RoR/mini_blog/db/schema.rb
+++ NB/RoR/mini_blog/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_06_092805) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_06_095418) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
