17e6047e1f1f58d5d6e032c9b43613c5d6d84cc7
Jean Porto <portojean@gmail.com>
2022-10-06 07:22:04 -0300

    add before_save

--- NB/RoR/mini_blog/app/models/post.rb
+++ NB/RoR/mini_blog/app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
     validates :title, :description, presence: true
     validates :title, length: { minimum:5 }
     validates :description, length: {maximum: 20}
+
+    before_save do 
+        self.title = self.title + " Jean Porto"
+    end
 end
