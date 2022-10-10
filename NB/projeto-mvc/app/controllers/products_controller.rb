bd16e8d369db6617e6025c89bf6865aa070fdaed
Jean Porto <portojean@gmail.com>
2022-10-10 18:57:56 -0300

    add def destroy

--- NB/projeto-mvc/app/controllers/products_controller.rb
+++ NB/projeto-mvc/app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-    before_action :set_product, only: [:show, :edit, :update]
+    before_action :set_product, only: [:show, :edit, :update, :destroy]
     
     def index
         @products = Product.all
@@ -29,6 +29,14 @@ class ProductsController < ApplicationController
             render :new, status: :unprocessable_entity
         end
     end
+
+    def destroy
+        if @product.destroy
+            redirect_to products_url
+        else
+            redirect_to @product
+        end
+    end 
 
     private 
 
