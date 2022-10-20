fe453b6f283ea1108cc24c257ca728fae90df185
Jean Porto <portojean@gmail.com>
2022-10-20 07:21:48 -0300

    feat palindromo

--- DIO/panlindromo.rb
+++ DIO/panlindromo.rb
@@ -1,5 +1,19 @@
 def palindromo?(palavra)
-    false
+    palavra_reversa = ""
+
+    indice = palavra.length 
+
+    until indice == 0
+        letra = palavra[indice -1]
+        palavra_reversa << letra
+        indice -= 1
+    end
+
+    if palavra_reversa == palavra
+        return true
+    else
+        return false
+    end
 end
 
 
