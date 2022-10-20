466b01b43ef1f08b5336fb5e65fc19b30e08ce65
Jean Porto <portojean@gmail.com>
2022-10-20 07:27:19 -0300

    refactor - abstraindo com reverse

--- DIO/panlindromo.rb
+++ DIO/panlindromo.rb
@@ -1,21 +1,8 @@
 def palindromo?(palavra)
-    palavra_reversa = ""
-
-    indice = palavra.length 
-
-    until indice == 0
-        letra = palavra[indice -1]
-        palavra_reversa << letra
-        indice -= 1
-    end
-
-    if palavra_reversa == palavra
-        return true
-    else
-        return false
-    end
+    #reverse - abstrai todo codigo anterior
+    palavra.downcase == palavra.downcase.reverse
 end
 
-
+puts palindromo?("Ovo")
 puts palindromo?("ovo")
 puts palindromo?("uff")
