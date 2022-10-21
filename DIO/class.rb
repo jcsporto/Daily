b91be3e092d83b5f458f85f91056689f747eb10f
Jean Porto <portojean@gmail.com>
2022-10-21 06:06:35 -0300

    entender metodos public, private e protected

---
+++ DIO/class.rb
@@ -0,0 +1,18 @@
+class MinhaClasse 
+    def m1
+        puts "Metodo 1"
+    end 
+
+    def m2
+        puts "Metodo 2"
+    end 
+
+    def m3
+        puts "Metodo 3"
+    end 
+end
+
+obj = MinhaClasse.new
+obj.m1
+obj.m2
+obj.m3
