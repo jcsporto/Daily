27b90f695754991816ff740947c5ef354fce770f
Jean Porto <portojean@gmail.com>
2022-10-21 06:39:30 -0300

    start principal

---
+++ DIO/OO/Conta Bancaria/principal.rb
@@ -0,0 +1,10 @@
+#principal 
+
+conta_jean = ContaBancaria.new("Jean", 100)
+conta_porto = ContaBancaria.new("Porto", 200)
+
+conta_jean.transferir(conta_porto, 50)
+
+p conta_jean.saldo #50
+p conta_porto.saldo #250
+
