baeb0a441df2d37c8895c720c36b41d11ba5ccbf
Jean Porto <portojean@gmail.com>
2022-11-02 10:33:36 -0300

    praticando

---
+++ Ambiente de Teste/hash/praticando.rb
@@ -0,0 +1,25 @@
+
+
+arr_a = []
+arr_a.push(89)
+p arr_a
+arr_a.push(777)
+arr_a.push("ruby")
+p arr_a
+
+
+arr_b = Array.new
+p arr_b
+arr_b.push(5678)
+arr_b.push("testes testes")
+p arr_b
+
+
+arr_c = %w(Curso de Ruby on Rails)
+p arr_c
+
+p arr_c[2]
+
+p arr_a.size.eql?(5)
+p arr_a.size.eql?(3)
+
