063af9588d417996c6e504a1727c8e8815bd4f62
Jean Porto <portojean@gmail.com>
2022-11-04 19:23:43 -0300

    add

--- Ambiente de Teste/t..rb
+++ Ambiente de Teste/t..rb
@@ -1,8 +1,20 @@
-#valor = 51
-#valor > 50 ? (puts "Eu sou maior que 50") : (puts "Eu sou menor que 50")
+# #valor = 51
+# #valor > 50 ? (puts "Eu sou maior que 50") : (puts "Eu sou menor que 50")
 
 
 
-for i in 0..9
-    puts "Valor de i #{i}"
+# # for i in 0..9
+# #     puts "Valor de i #{i}"
+# # end
+
+
+# # #$, = ", "
+# range1 = (1..10).to_a
+# range2 = ('baa'..'baj').to_a
+
+# puts "#{range1}"
+# puts "#{range2}"
+
+(0..5).each do |i|
+    print "valor #{i} "
 end
