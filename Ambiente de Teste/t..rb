8971edda26494d064797ab432ca66b1be7479a06
Jean Porto <portojean@gmail.com>
2022-11-04 05:48:15 -0300

    for in

--- Ambiente de Teste/t..rb
+++ Ambiente de Teste/t..rb
@@ -1,2 +1,8 @@
-valor = 51
-valor > 50 ? (puts "Eu sou maior que 50") : (puts "Eu sou menor que 50")
+#valor = 51
+#valor > 50 ? (puts "Eu sou maior que 50") : (puts "Eu sou menor que 50")
+
+
+
+for i in 0..9
+    puts "Valor de i #{i}"
+end
