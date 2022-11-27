b8556deb5a34300a2865f5e32ae921418b228873
Jean Porto <portojean@gmail.com>
2022-11-27 10:17:02 -0300

    refactor: calculadora

--- RubyPuro/03/iterações/calc.rb
+++ RubyPuro/03/iterações/calc.rb
@@ -11,6 +11,7 @@ loop do
 
     puts "Opção"
     option = gets.chomp.to_i
+    break if option == 0
 
     print 'ok, agora digite o primeiro numero:'
     num1 = gets.chomp.to_i
@@ -18,27 +19,28 @@ loop do
     print 'a vez do do segundo numero:'
     num2 = gets.chomp.to_i
 
-    case option
-    when 1
-        resultado = num1 + num2
-        print "Resultado: #{num1} + #{num2} = #{resultado}"
-    
-    when 2
-        resultado = num1 - num2
-        print "Resultado: #{num1} - #{num2} = #{resultado}"
+    def opr(option, num1, num2)
+        @option = option
+        @num1 = num1
+        @num2 = num2
 
-    when 3
-        resultado = num1 * num2
-        print "Resultado: #{num1} * #{num2} = #{resultado}"
-
-    when 4
-        resultado = num1 / num2
-        print "Resultado: #{num1} / #{num2} = #{resultado}"
-    when 0
-        break
-    else 
-        puts "não foi possivel indentificar"
+        case @option
+        when 1
+            resultado = @num1 + @num2
+        when 2 
+            resultado = @num1 - @num2
+        when 3 
+            resultado = @num1 * @num2
+        when 4 
+            resultado = @num1 / @num2   
+        else
+            puts "Tente novamente"            
+        end
     end
+    
+    p "Resultado: #{opr(option, num1, num2)}"
+   
+    
     
     #Comando que limp o console
     # system "clear"
