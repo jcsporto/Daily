1f2c6cb8414f5e1d7853f67a1ae5986503781d34
Jean Porto <portojean@gmail.com>
2022-11-28 19:41:54 -0300

    feat: add gem OS

---
+++ RubyPuro/05 - Métodos e Gems/os.rb
@@ -0,0 +1,15 @@
+require 'os'
+
+def my_os
+    if OS.windows?
+        "windows"
+    elsif OS.linux?
+        "Linux"
+    elsif OS.mac?
+        "Osx"
+    else
+        "Não consegui identificar"
+    end    
+end
+
+puts "Meu PC possui #{OS.cpu_count} cores, é #{OS.bits} e o sistema operacional é #{my_os}"
