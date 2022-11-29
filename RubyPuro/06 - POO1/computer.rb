5beb2b994d3b9d4f9cdf62ef3014156492d46172
Jean Porto <portojean@gmail.com>
2022-11-29 05:37:05 -0300

    feat: add classe Computer

---
+++ RubyPuro/06 - POO1/computer.rb
@@ -0,0 +1,13 @@
+class Computer 
+    def turn_on
+        'turn on the computer'
+    end
+
+    def shutdow
+        'shutdown the computer'
+    end
+end
+
+computer = Computer.new
+p computer.shutdow
+
