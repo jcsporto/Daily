c5127bc755ad2e6a1085bfbdcb1e4ef3f5d1ae35
Jean Porto <portojean@gmail.com>
2022-11-29 06:29:29 -0300

    feat: add novo objeto

--- RubyPuro/06 - POO1/Animal/app.rb
+++ RubyPuro/06 - POO1/Animal/app.rb
@@ -1,4 +1,11 @@
-require_relative './animal.rb'
+require_relative 'animal.rb'
+require_relative 'cachorro.rb'
 
+p 'Animal'
 animal = Animal.new
 animal.pular
+
+p 'Cachorro'
+cachorro = Cachorro.new
+cachorro.pular
+cachorro.latir
