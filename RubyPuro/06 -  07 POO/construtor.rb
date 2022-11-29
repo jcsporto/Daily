6b1545208482d74618ea5a649b168d21aa8c8c5b
Jean Porto <portojean@gmail.com>
2022-11-29 07:37:16 -0300

    feat: add constructor

---
+++ RubyPuro/06 -  07 POO/construtor.rb
@@ -0,0 +1,16 @@
+class Person
+
+    def initialize(name, age)
+        @name = name
+        @age = age
+    end
+
+    def check 
+        puts "Instacia da classe iniciada com os valores:"
+        puts "Name = #{@name}"
+        puts "Idade = #{@age}"
+    end
+end
+
+person = Person.new('João', 12)
+person.check
