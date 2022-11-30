3bd75053177d114fd9e8aa90e4d0f9abb46db49f
Jean Porto <portojean@gmail.com>
2022-11-30 08:53:57 -0300

    feat: ->  lambda

--- RubyPuro/08 /lambda.rb
+++ RubyPuro/08 /lambda.rb
@@ -1,3 +1,6 @@
- first_lambda = lambda {puts "my first lambda"}
- first_lambda.call
- first_lambda.call
+#  first_lambda = lambda {puts "my first lambda"}
+#  first_lambda.call
+
+# outra sintaxe para lambda ->
+first_lambda = -> {puts "my first lambda"}
+first_lambda.call
