5ec588173f9ca3d9142605f2c69b41050e36981a
Jean Porto <portojean@gmail.com>
2022-11-30 09:06:35 -0300

    feat: add lambda com multiplas linhas

--- RubyPuro/08 /lambda.rb
+++ RubyPuro/08 /lambda.rb
@@ -5,9 +5,28 @@
 # first_lambda = -> {puts "my first lambda"}
 # first_lambda.call
 
-#Lambda com parâmetros
-first_lambda = -> (names) {names.each {|name| puts name}}
+# #Lambda com parâmetros
+# first_lambda = -> (names) {names.each {|name| puts name }}
 
-names = ["João", "Maria", "Pedro"]
+# names = ["João", "Maria", "Pedro"]
 
-first_lambda.call(names)
+# first_lambda.call(names)
+
+
+#Lambda de múltiplas linhas
+my_lambda = lambda do |numbers|
+    index = 0
+    puts 'Numero atual + Proximo número'
+    numbers.each do |number|
+        return if numbers[index] == numbers.last
+        puts "(#{numbers[index]}) + (#{numbers[index + 1]})"
+        puts numbers[index] + numbers[index + 1 ]
+        index += 1
+    end
+end
+
+numbers = [1, 2, 3, 4]
+my_lambda.call(numbers)
+
+numbers = [8, 9, 3, 4]
+my_lambda.call(numbers)
