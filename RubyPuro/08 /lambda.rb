8ad79c03c2069876311d1cb35f20d30ebd9cd7f6
Jean Porto <portojean@gmail.com>
2022-11-30 08:58:40 -0300

    feat: add lambda com parametro

--- RubyPuro/08 /lambda.rb
+++ RubyPuro/08 /lambda.rb
@@ -2,5 +2,12 @@
 #  first_lambda.call
 
 # outra sintaxe para lambda ->
-first_lambda = -> {puts "my first lambda"}
-first_lambda.call
+# first_lambda = -> {puts "my first lambda"}
+# first_lambda.call
+
+#Lambda com parâmetros
+first_lambda = -> (names) {names.each {|name| puts name}}
+
+names = ["João", "Maria", "Pedro"]
+
+first_lambda.call(names)
