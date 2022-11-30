cb54514eef36370cfef5754101048d0c8969fd51
Jean Porto <portojean@gmail.com>
2022-11-30 09:10:32 -0300

    feat: add lambda como argumentos de metodo

--- RubyPuro/08 /lambda.rb
+++ RubyPuro/08 /lambda.rb
@@ -1,6 +1,8 @@
 #  first_lambda = lambda {puts "my first lambda"}
 #  first_lambda.call
 
+
+#---------------------------------------------------
 # outra sintaxe para lambda ->
 # first_lambda = -> {puts "my first lambda"}
 # first_lambda.call
@@ -13,20 +15,33 @@
 # first_lambda.call(names)
 
 
+#---------------------------------------------------
 #Lambda de múltiplas linhas
-my_lambda = lambda do |numbers|
-    index = 0
-    puts 'Numero atual + Proximo número'
-    numbers.each do |number|
-        return if numbers[index] == numbers.last
-        puts "(#{numbers[index]}) + (#{numbers[index + 1]})"
-        puts numbers[index] + numbers[index + 1 ]
-        index += 1
-    end
+# my_lambda = lambda do |numbers|
+#     index = 0
+#     puts 'Numero atual + Proximo número'
+#     numbers.each do |number|
+#         return if numbers[index] == numbers.last
+#         puts "(#{numbers[index]}) + (#{numbers[index + 1]})"
+#         puts numbers[index] + numbers[index + 1 ]
+#         index += 1
+#     end
+# end
+
+# numbers = [1, 2, 3, 4]
+# my_lambda.call(numbers)
+
+# numbers = [8, 9, 3, 4]
+# my_lambda.call(numbers)
+
+#---------------------------------------------------
+#Lambda como argumento de método
+def foo(first_lambda, second_lambda)
+    first_lambda.call
+    second_lambda.call
 end
 
-numbers = [1, 2, 3, 4]
-my_lambda.call(numbers)
+first_lambda = lambda {puts "my first lambda"}
+second_lambda = lambda {puts "my second lambda"}
 
-numbers = [8, 9, 3, 4]
-my_lambda.call(numbers)
+foo(first_lambda, second_lambda)
