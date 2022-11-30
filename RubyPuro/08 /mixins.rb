c4449a8f422345c0d2c304bba6f55ca9f1b2e9e4
Jean Porto <portojean@gmail.com>
2022-11-30 09:50:31 -0300

    feat: create modulos

--- RubyPuro/08 /mixins.rb
+++ RubyPuro/08 /mixins.rb
@@ -7,3 +7,33 @@ module ImpressaoDecorada
         puts decoracao
     end
 end
+
+module Pernas 
+    include ImpressaoDecorada
+
+    def chute_frontal
+        imprimir 'Chute Frontal'
+    end
+
+    def chute_lateral
+        imprimir 'Chute Lateral'
+    end
+end
+
+module Bracos
+    include ImpressaoDecorada
+
+    def jab_de_direita 
+        imprimi 'Jab de direita'
+    end
+
+    def jab_de_esquerda
+        imprimi 'Jab de esquerda'
+    end
+
+    def gancho
+        imprimi 'Gancho'
+    end
+end
+
+
