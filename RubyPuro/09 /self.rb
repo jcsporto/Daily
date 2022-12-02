0e16e82ab1cd9c0aad7279fb3e6251d8e2445585
Jean Porto <portojean@gmail.com>
2022-12-01 21:14:20 -0300

    feat: class Pen

--- RubyPuro/09 /self.rb
+++ RubyPuro/09 /self.rb
@@ -1,8 +1,20 @@
-class Foo
-    def self.bar
-        puts self
+# class Foo
+#     def self.bar
+#         puts self
+#     end
+# end
+
+# Foo.bar
+
+
+class Pen
+    attr_accessor :color
+    
+    def pen_color 
+        puts "The color is " + self.color
     end
 end
 
-Foo.bar
-
+pen = Pen.new
+pen.color = "blue"
+pen.pen_color
